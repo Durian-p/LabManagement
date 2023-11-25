ee709c893820658c05455d4f6c67021766c9a827
Zifeng Tan <tanzifeng1030@163.com>
2023-11-25 22:28:31 +0800

    完成人员相关API

--- src/main/java/com/example/exp2/controller/StaffController.java
+++ src/main/java/com/example/exp2/controller/StaffController.java
@@ -4,9 +4,7 @@ import com.example.exp2.common.CommonResponse;
 import com.example.exp2.pojo.Staff;
 import com.example.exp2.service.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,29 @@ public class StaffController
     public CommonResponse getAllStaffs()
     {
         return CommonResponse.forSuccess("成功获取员工列表",staffService.getAllStaffs());
+    }
+
+    @GetMapping("{id}")
+    public CommonResponse getStaffById(@PathVariable("id") String id)
+    {
+        return CommonResponse.forSuccess("成功获取员工信息",staffService.getStaffById(id));
+    }
+
+    @PostMapping
+    public CommonResponse addStaff(@RequestBody Staff staff)
+    {
+        return CommonResponse.forSuccess("成功添加员工信息",staffService.addStaff(staff));
+    }
+
+    @PatchMapping("{id}")
+    public CommonResponse updateStaffById(@PathVariable("id") String id, @RequestBody Staff staff)
+    {
+        return CommonResponse.forSuccess("成功更新员工信息",staffService.updateStaffById(id,staff));
+    }
+
+    @DeleteMapping("{id}")
+    public CommonResponse deleteStaffById(@PathVariable("id") String id)
+    {
+        return CommonResponse.forSuccess("成功删除员工信息",staffService.deleteStaffById(id));
     }
 }
--- src/main/java/com/example/exp2/service/StaffService.java
+++ src/main/java/com/example/exp2/service/StaffService.java
@@ -1,6 +1,7 @@
 package com.example.exp2.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.exp2.pojo.Staff;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.exp2.Mapper.StaffMapper;
@@ -22,5 +23,35 @@ public class StaffService
     {
         QueryWrapper queryWrapper = new QueryWrapper();
         return mapper.selectList(queryWrapper);
+    }
+
+    public Staff getStaffById(String id)
+    {
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("staffId",id);
+        return mapper.selectOne(queryWrapper);
+    }
+
+    public Integer addStaff(Staff staff)
+    {
+        mapper.insert(staff);
+        return staff.getStaffId();
+    }
+
+    public Staff updateStaffById(String id, Staff staff)
+    {
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("staffId",id);
+        mapper.update(staff,queryWrapper);
+        return mapper.selectOne(queryWrapper);
+    }
+
+    public Staff deleteStaffById(String id)
+    {
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("staffId",id);
+        Staff staff = mapper.selectOne(queryWrapper);
+        mapper.delete(queryWrapper);
+        return staff;
     }
 }
